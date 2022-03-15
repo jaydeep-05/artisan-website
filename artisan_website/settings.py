395107d2028c8f733c03296e6d19880968a677a5
Kri-hika <73770973+Kri-hika@users.noreply.github.com>
2022-03-15 11:27:51 +0530

    Feat(Models.js) : updated

--- artisan_website/settings.py
+++ artisan_website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,11 +78,12 @@ WSGI_APPLICATION = 'artisan_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'artisan-website',
-        'HOST':'localhost',
-        'USER':'postgres',
-        'PASSWORD':'root'
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'artisan_website',
+        'USER':'root',
+        'PASSWORD':'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -120,8 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
